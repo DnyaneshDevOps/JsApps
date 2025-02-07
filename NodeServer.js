6805929dd88dcd68cc34fd361a19b5d53585b11d
dnyanesh-ai <dnyaneshwar.rajmane@visnet.in>
2025-02-07 12:34:44 +0530

    Update NodeServer.js

--- NodeServer.js
+++ NodeServer.js
@@ -1,11 +1,7 @@
 const http = require('http');
 
 const hostname = '127.0.0.1';
-<<<<<<< HEAD
-const port = 5000;
-=======
 const port = 3000;
->>>>>>> 131d48a148a4603f9c63f4e75dd7fbe0c64682ac
 
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
